810595d4e33c6023ec6b73f0ff661c012d378e5d
Paul-Nicolas Madelaine <pnm@pnm.tf>
2023-09-19 00:51:58 +0200

    update script

--- apply-www.sh
+++ apply-www.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -eu
 
-NIX_SSHOPTS=-t nixos-rebuild switch --flake .#www --target-host pnm@92.243.26.156 --use-remote-sudo
+NIX_SSHOPTS=-t nixos-rebuild switch --flake .#www --target-host pnm@pnm.tf --use-remote-sudo
