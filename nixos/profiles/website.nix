e28f1990e0cbd34547bb51a5c23df85319474bd2
Paul-Nicolas Madelaine <pnm@pnm.tf>
2023-09-19 00:21:55 +0200

    enable ssl

--- nixos/profiles/website.nix
+++ nixos/profiles/website.nix
@@ -5,11 +5,18 @@
   services.nginx = {
     enable = true;
     virtualHosts."pnm.tf" = {
+      addSSL = true;
+      enableACME = true;
       root = config.inputs.website.packages.${pkgs.system}.default;
       serverAliases = [ "www.pnm.tf" ];
     };
   };
 
-  networking.firewall.allowedTCPPorts = [ 80 ];
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "pnm@pnm.tf";
+  };
+
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
 
 }
