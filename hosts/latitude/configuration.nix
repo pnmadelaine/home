cd372270c1819b9a133333cc8e1e8533fd25ef2f
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-04-06 14:28:49 +0200

    enable steam

--- hosts/latitude/configuration.nix
+++ hosts/latitude/configuration.nix
@@ -50,6 +50,9 @@
 
   security.pam.services.swaylock = {text = "auth include login";};
 
+  programs.steam.enable = true;
+  nixpkgs.config.allowUnfree = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
